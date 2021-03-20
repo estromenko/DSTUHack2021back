d33f41df0438f5c512ede4ab54f074eaa7357bc2
A-ST-RA <astrax200469@gmail.com>
2021-03-21 02:02:29 +0300

    Добавил пополнение баланса для пользователя

--- internal/server/userHandlers.go
+++ internal/server/userHandlers.go
@@ -4,6 +4,7 @@ import (
 	"dstuhack/internal/models"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 func (s *Server) GetUserInfo() http.HandlerFunc {
@@ -69,5 +70,40 @@ func (s *Server) GetUserPortfolio() http.HandlerFunc {
 
 		w.WriteHeader(200)
 		json.NewEncoder(w).Encode(portfolio)
+	}
+}
+
+
+func (s *Server) UpdateUserBalanceById() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, ok := r.Context().Value("user").(*models.User)
+		if !ok {
+			w.WriteHeader(403)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"error": "Not authorized",
+			})
+			return
+		}
+		
+		strBalance := r.PostFormValue("balance")
+
+		balance, err := strconv.ParseFloat(strBalance, 32)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		user.Balance = float32(balance)
+
+		if err := s.db.User().Update(user); err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"error": err.Error(),
+			})
+			return
+		}
 	}
 }
