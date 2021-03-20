647a1ebe277f479023139a529404a66b2a691030
A-ST-RA <astrax200469@gmail.com>
2021-03-20 10:01:06 +0300

    АААААА

--- internal/db/operationRepo.go
+++ internal/db/operationRepo.go
@@ -19,7 +19,7 @@ func NewOperationRepo(db *sql.DB) *OperationRepo {
 
 // Create operation
 func (u *OperationRepo) Create(operation *models.Operation) error {
-	if err := u.db.QueryRow(`INSERT INTO operations (user_id, type, name, purchase_price, amount) VALUES ($1, $2, $3, $4, $5)`,
+	if err := u.db.QueryRow(`INSERT INTO operations (user_id, type, name, purchase_price, amount) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
 		&operation.UserId,
 		&operation.Type,
 		&operation.Name,
@@ -36,6 +36,45 @@ func (u *OperationRepo) GetAllByUserId(userId int) ([]*models.Operation, error) 
 	rows, _:= u.db.Query(`SELECT * FROM operations WHERE user_id = $1`, userId)
 
 	var operations []*models.Operation
+	for rows.Next() {
+		var (
+			id       	  int
+			userId   	  int
+			_type	 	  string
+			name 	 	  string
+			purchasePrice float32
+			amount		  int
+		)
+
+		if err := rows.Scan(&id, 
+			&userId, 
+			&_type, 
+			&name, 
+			&purchasePrice, 
+			&amount,
+			); err != nil {
+			return nil, err
+		}
+		_operation := &models.Operation{
+			ID: id,
+			UserId: userId,
+			Type: _type,
+			Name: name,
+	 		PurchasePrice: purchasePrice,
+			Amount: amount,
+		}
+
+		operations = append(operations, _operation)
+	}
+	return operations, nil
+}
+
+
+// Валюта, акция, облигация
+func (u *OperationRepo) GetAllUserValute(searchType string, id int) []*models.Opertaion {
+	rows, _ := u.db.Query(`SELECET * FROM operations WHERE type = $1 AND userId = $2`, searchType, id)
+
+	var opertaions []*models.Opertaion
 	for rows.Next() {
 		var (
 			id       	  int
